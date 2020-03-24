8a8a596c2743b075487166ae635fe945e0e299a8
Cory House <housecor@gmail.com>
2020-03-24 14:39:48 -0500

    Delete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { get1099s } from "./api/f1099api";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  const f1099s = [
-    { ein: 1, employer: "Facebook", wages: 300000, withheld: 50000 },
-    { ein: 2, employer: "Apple", wages: 240000, withheld: 33000 },
-    { ein: 3, employer: "Amazon", wages: 60000, withheld: 15000 }
-  ];
+const default1099s = [
+  { ein: 1, employer: "Facebook", wages: 300000, withheld: 50000 },
+  { ein: 2, employer: "Apple", wages: 240000, withheld: 33000 },
+  { ein: 3, employer: "Amazon", wages: 60000, withheld: 15000 }
+];
 
+function App() {
+  const [f1099s, setF1099s] = useState(default1099s);
+
+  function onDeleteClick(ein) {
+    const new1099s = f1099s.filter(f => f.ein !== ein);
+    setF1099s(new1099s);
+  }
+
+  // HTML is a representation of application state, not a source of truth.
   function render1099(f1099) {
     // object destructuring
     const { ein, employer, wages, withheld } = f1099;
     return (
       <tr>
         <td>
-          <button className="btn btn-danger">Delete</button>
+          <button onClick={() => onDeleteClick(ein)} className="btn btn-danger">
+            Delete
+          </button>
         </td>
         <td>{ein}</td>
         <td>{employer}</td>
@@ -32,7 +42,7 @@ function App() {
         <thead>
           <tr>
             <th></th>
-            <th>EID</th>
+            <th>EIN</th>
             <th>Employer</th>
             <th>Wages</th>
             <th>Withheld</th>
