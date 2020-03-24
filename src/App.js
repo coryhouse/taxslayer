922d22365c1aacf4f22e242ee5cca35eef4592f2
Cory House <housecor@gmail.com>
2020-03-24 13:01:25 -0500

    table

--- src/App.js
+++ src/App.js
@@ -7,7 +7,36 @@ function App() {
     { ein: 3, employer: "Amazon", wages: 60000, withheld: 15000 }
   ];
 
-  return <h1>1099s</h1>;
+  function render1099(f1099) {
+    return (
+      <tr>
+        <td>{f1099.ein}</td>
+        <td>{f1099.employer}</td>
+        <td>{f1099.wages}</td>
+        <td>{f1099.withheld}</td>
+      </tr>
+    );
+  }
+
+  // Exercise 1:
+  // Display ein, employer, wages, and withheld in a table.
+  // Display a header for each.
+  return (
+    <>
+      <h1>1099s</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>EID</th>
+            <th>Employer</th>
+            <th>Wages</th>
+            <th>Withheld</th>
+          </tr>
+        </thead>
+        <tbody>{f1099s.map(render1099)}</tbody>
+      </table>
+    </>
+  );
 }
 
 export default App;
