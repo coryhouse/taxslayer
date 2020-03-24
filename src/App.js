d16ac8b66dc12965a60b1666d0737a2df8ec8e27
Cory House <housecor@gmail.com>
2020-03-24 12:21:42 -0500

    add 1099s

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  const f1099s = [
+    { ein: 1, employer: "Facebook", wages: 300000, withheld: 50000 },
+    { ein: 2, employer: "Apple", wages: 240000, withheld: 33000 },
+    { ein: 3, employer: "Amazon", wages: 60000, withheld: 15000 }
+  ];
+
+  return <h1>1099s</h1>;
+}
+
+export default App;
