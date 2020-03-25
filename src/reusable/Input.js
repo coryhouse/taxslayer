eef58118bc1d22ef56e4a7ac66c41748354c43a5
Cory House <housecor@gmail.com>
2020-03-25 15:03:13 -0500

    add propTypes and required prop

--- src/reusable/Input.js
+++ src/reusable/Input.js
@@ -1,19 +1,40 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 // Desturcturing props
-const Input = ({ id, label, value, onChange }) => {
+const Input = ({ id, label, value, onChange, required, onBlur }) => {
   return (
     <div className="form-group">
-      <label htmlFor={id}>{label}</label>
+      <label htmlFor={id}>
+        {label} {required && "*"}
+      </label>
       <br />
       <input
         id={id}
+        onBlur={onBlur}
         className="form-control"
         onChange={onChange}
         value={value}
       />
     </div>
   );
+};
+
+// Document the properties that this component accepts
+Input.propTypes = {
+  /** HTML ID */
+  id: PropTypes.string.isRequired,
+
+  /** Input label */
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func.isRequired,
+  required: PropTypes.bool
+};
+
+Input.defaultProps = {
+  required: false
 };
 
 export default Input;
