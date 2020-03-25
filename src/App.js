6bc360968f9185c6cd366d8b08e7681be5ae21ef
Cory House <housecor@gmail.com>
2020-03-25 14:14:38 -0500

    add working

--- src/App.js
+++ src/App.js
@@ -29,9 +29,11 @@ function App() {
 
   function handleSubmit(event) {
     event.preventDefault(); // don't reload the page
-    f1099api.add1099(f1099).then(() => {
+    f1099api.add1099(f1099).then(response => {
+      const saved1099 = response.data; // this has the new ID assigned
       // this runs after successful add
       setF1099(new1099); // reset form
+      setF1099s([...f1099s, saved1099]); // add new 1099 to the list
     });
   }
 
