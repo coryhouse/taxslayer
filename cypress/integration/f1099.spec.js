fea42936489147a8e885492bd4c2c9b8871c6480
Cory House <housecor@gmail.com>
2020-03-25 14:00:31 -0500

    Enhance test

--- cypress/integration/f1099.spec.js
+++ cypress/integration/f1099.spec.js
@@ -13,6 +13,12 @@ context("F1099s", () => {
     cy.get("form").within(() => {
       // all queries we make inside of here will only look in the form.
       cy.findByText("Add 1099").click();
+
+      // Now the form should be reset
+      cy.findByLabelText("EIN").should("have.value", "");
+      cy.findByLabelText("Employer").should("have.value", "");
+      cy.findByLabelText("Wages").should("have.value", "");
+      cy.findByLabelText("Withheld").should("have.value", "");
     });
     cy.findByLabelText("Delete test Employer 1099").click(); // click delete on the first record.
   });
