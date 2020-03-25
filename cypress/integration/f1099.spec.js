5d907b16a11a18547fd9a7c2ef642457f34beb42
Cory House <housecor@gmail.com>
2020-03-25 12:47:20 -0500

    fill form via cy

--- cypress/integration/f1099.spec.js
+++ cypress/integration/f1099.spec.js
@@ -5,7 +5,15 @@ context("F1099s", () => {
     cy.visit("http://localhost:3000");
   });
 
-  it("should delete Amazon 1099 when delete is clicked", () => {
-    cy.findByLabelText("Delete Amazon 1099").click(); // click delete on the first record.
+  it("should add and delete a new record", () => {
+    cy.findByLabelText("EIN").type("test EIN");
+    cy.findByLabelText("Employer").type("test Employer");
+    cy.findByLabelText("Wages").type("100");
+    cy.findByLabelText("Withheld").type("200");
+    cy.get("form").within(() => {
+      // all queries we make inside of here will only look in the form.
+      cy.findByText("Add 1099").click();
+    });
+    //cy.findByLabelText("Delete Amazon 1099").click(); // click delete on the first record.
   });
 });
