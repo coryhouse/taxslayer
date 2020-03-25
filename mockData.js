fe8d2119efe626d7688add826586e8204f2c7cd1
Cory House <housecor@gmail.com>
2020-03-25 10:14:04 -0500

    Add mockData

---
+++ mockData.js
@@ -0,0 +1,27 @@
+const f1099s = [
+  {
+    id: 1,
+    ein: "13-385835",
+    employer: "Facebook",
+    wages: 300000,
+    withheld: 50000
+  },
+  {
+    id: 2,
+    ein: "32-1835858",
+    employer: "Apple",
+    wages: 240000,
+    withheld: 33000
+  },
+  {
+    id: 3,
+    ein: "82-823838",
+    employer: "Amazon",
+    wages: 60000,
+    withheld: 15000
+  }
+];
+
+module.exports = {
+  f1099s: f1099s
+};
