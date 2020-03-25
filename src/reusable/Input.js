4fc4a493e7d1d8152b34b5db0d64c75ca0790095
Cory House <housecor@gmail.com>
2020-03-25 13:40:01 -0500

    add props

--- src/reusable/Input.js
+++ src/reusable/Input.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 // Desturcturing props
-const Input = ({ id, label }) => {
+const Input = ({ id, label, value, onChange }) => {
   return (
     <div className="form-group">
       <label htmlFor={id}>{label}</label>
       <br />
-      <input id={id} className="form-control" />
+      <input
+        id={id}
+        className="form-control"
+        onChange={onChange}
+        value={value}
+      />
     </div>
   );
 };
