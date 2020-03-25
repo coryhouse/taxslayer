87a0fa4a25aaa370aa3b4c6e3f1ea733f407cd22
Cory House <housecor@gmail.com>
2020-03-25 15:09:48 -0500

    display error

--- src/reusable/Input.js
+++ src/reusable/Input.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 // Desturcturing props
-const Input = ({ id, label, value, onChange, required, onBlur }) => {
+const Input = ({ id, label, value, onChange, required, onBlur, error }) => {
   return (
     <div className="form-group">
       <label htmlFor={id}>
@@ -16,6 +16,7 @@ const Input = ({ id, label, value, onChange, required, onBlur }) => {
         onChange={onChange}
         value={value}
       />
+      {error && <div>{error}</div>}
     </div>
   );
 };
@@ -24,6 +25,9 @@ const Input = ({ id, label, value, onChange, required, onBlur }) => {
 Input.propTypes = {
   /** HTML ID */
   id: PropTypes.string.isRequired,
+
+  /** Specify an error to display below the input */
+  error: PropTypes.string,
 
   /** Input label */
   label: PropTypes.string.isRequired,
