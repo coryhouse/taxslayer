3681b888b4ace9bf8943d5f2205fba53cb55ea10
Cory House <housecor@gmail.com>
2020-03-25 12:58:37 -0500

    add1099 func

--- src/api/f1099api.js
+++ src/api/f1099api.js
@@ -13,3 +13,7 @@ export function get1099s() {
 export function delete1099(id) {
   return axios.delete(`http://localhost:3001/f1099s/${id}`);
 }
+
+export function add1099(f1099) {
+  return axios.post("http://localhost:3001/f1099s", f1099);
+}
