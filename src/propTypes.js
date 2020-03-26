241260ad83408e721f70ef59ad6fb6d666e12f71
Cory House <housecor@gmail.com>
2020-03-26 12:31:37 -0500

    Centralize propTypes

---
+++ src/propTypes.js
@@ -0,0 +1,10 @@
+// Centralizing reused propTypes here to avoid repeating myself.
+import PropTypes from "prop-types";
+
+export const f1099Type = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  ein: PropTypes.string.isRequired,
+  employer: PropTypes.string.isRequired,
+  wages: PropTypes.string.isRequired,
+  withheld: PropTypes.string.isRequired
+});
