023a66dace78243b6d7bc335b8a7f91a528fa784
Cory House <housecor@gmail.com>
2020-03-26 15:00:52 -0500

    handle add vs edit and parseInt

--- src/Manage1099.js
+++ src/Manage1099.js
@@ -17,7 +17,7 @@ const new1099 = {
 function Manage1099({ f1099s, setF1099s }) {
   const history = useHistory();
   const match = useRouteMatch();
-  const idToEdit = match.params.id;
+  const idToEdit = parseInt(match.params.id);
   const [f1099, setF1099] = useState(new1099); // holds add 1099 form
   const [errors, setErrors] = useState({});
   useEffect(load1099s, []); // call this function immediately after the first render
@@ -37,17 +37,22 @@ function Manage1099({ f1099s, setF1099s }) {
   }
 
   function get1099ById(f1099s, id) {
-    const f1099ToEdit = f1099s.find(d => d.id === parseInt(id));
+    const f1099ToEdit = f1099s.find(d => d.id === id);
     if (!f1099ToEdit) return history.push("/page-not-found");
     return f1099ToEdit;
   }
 
   function handleSubmit(event) {
     event.preventDefault(); // don't reload the page
-    f1099api.add1099(f1099).then(response => {
+    f1099api.save1099(f1099).then(response => {
       const saved1099 = response.data; // this has the new ID assigned
-      // this runs after successful add
-      setF1099s([...f1099s, saved1099]); // add new 1099 to the list
+      // handle add vs edit
+      if (idToEdit) {
+        const new1099s = f1099s.map(f => (f.id === idToEdit ? saved1099 : f));
+        setF1099s(new1099s);
+      } else {
+        setF1099s([...f1099s, saved1099]); // add new 1099 to the list
+      }
       history.push("/f1099s");
     });
   }
