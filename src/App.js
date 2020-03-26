dc963c1d405feeab689acd284d65b50e6df5595f
Cory House <housecor@gmail.com>
2020-03-26 10:02:21 -0500

    use container

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Home from "./Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Grid from "./Grid";
 import Manage1099 from "./Manage1099";
+import Nav from "./Nav";
 
 function App() {
   return (
-    <>
+    <div className="container">
+      <Nav />
       <Route path="/" component={Home} exact />
       <Route path="/f1099s" component={Grid} />
       <Route path="/manage" component={Manage1099} />
-    </>
+    </div>
   );
 }
 
