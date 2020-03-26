85956f9c3096d40490bfcd0feff21a8330daffad
Cory House <housecor@gmail.com>
2020-03-26 10:42:44 -0500

    update test

--- cypress/integration/f1099.spec.js
+++ cypress/integration/f1099.spec.js
@@ -1,11 +1,8 @@
 /// <reference types="cypress" />
 
 context("F1099s", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000");
-  });
-
   it("should display validation onBlur of empty fields", () => {
+    cy.visit("http://localhost:3000/manage");
     cy.findByLabelText("EIN *")
       .focus()
       .blur();
@@ -26,6 +23,7 @@ context("F1099s", () => {
   });
 
   it("should add and delete a new record", () => {
+    cy.visit("http://localhost:3000/manage");
     cy.findByLabelText("EIN *").type("test EIN");
     cy.findByLabelText("Employer *").type("test Employer");
     cy.findByLabelText("Wages *").type("100");
@@ -33,13 +31,8 @@ context("F1099s", () => {
     cy.get("form").within(() => {
       // all queries we make inside of here will only look in the form.
       cy.findByText("Add 1099").click();
-
-      // Now the form should be reset
-      cy.findByLabelText("EIN *").should("have.value", "");
-      cy.findByLabelText("Employer *").should("have.value", "");
-      cy.findByLabelText("Wages *").should("have.value", "");
-      cy.findByLabelText("Withheld *").should("have.value", "");
     });
+    // App should have redirected to the grid page, so delete button for new record should be visible now.
     cy.findByLabelText("Delete test Employer 1099").click(); // click delete on the first record.
     cy.findByText("1099 Deleted");
   });
