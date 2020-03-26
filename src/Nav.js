58fbab9a62e545f39855c4b8a01fb68fe2f4f4a6
Cory House <housecor@gmail.com>
2020-03-26 10:25:33 -0500

    style active link

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const activeStyle = {
+  color: "orange"
+};
+
 function Nav() {
   return (
     <>
-      <NavLink to="/">Home</NavLink> | <NavLink to="/f1099s">1099s</NavLink>
+      <NavLink activeStyle={activeStyle} to="/" exact>
+        Home
+      </NavLink>{" "}
+      |{" "}
+      <NavLink activeStyle={activeStyle} to="/f1099s">
+        1099s
+      </NavLink>
     </>
   );
 }
