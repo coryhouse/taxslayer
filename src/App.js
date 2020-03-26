ca237725bd182732ad11e32fcb2bb05a37e444c4
Cory House <housecor@gmail.com>
2020-03-26 14:03:21 -0500

    Add 404 route via switch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Grid from "./Grid";
 import Manage1099 from "./Manage1099";
 import Nav from "./Nav";
+import PageNotFound from "./PageNotFound";
 
 function App() {
   const [f1099s, setF1099s] = useState([]); // holds list of 1099s
@@ -12,14 +13,17 @@ function App() {
   return (
     <div className="container">
       <Nav />
-      <Route path="/" component={Home} exact />
-      <Route path="/f1099s">
-        <Grid f1099s={f1099s} setF1099s={setF1099s} />
-      </Route>
-      {/* Last segment of URL is optional. It holds the 1099 ID if populated. */}
-      <Route path="/manage/:id?">
-        <Manage1099 f1099s={f1099s} setF1099s={setF1099s} />
-      </Route>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/f1099s">
+          <Grid f1099s={f1099s} setF1099s={setF1099s} />
+        </Route>
+        {/* Last segment of URL is optional. It holds the 1099 ID if populated. */}
+        <Route path="/manage/:id?">
+          <Manage1099 f1099s={f1099s} setF1099s={setF1099s} />
+        </Route>
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
