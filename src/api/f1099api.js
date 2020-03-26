2d0fb6d8fd0cdf249918a13e493ff8e8ed4eaa70
Cory House <housecor@gmail.com>
2020-03-26 15:45:28 -0500

    Add async

--- src/api/f1099api.js
+++ src/api/f1099api.js
@@ -5,16 +5,16 @@ import axios from "axios";
 // 2. Promise
 // 3. Async/await (abstraction over promises)
 
-export function get1099s() {
+export async function get1099s() {
   // This will return a promise
   return axios.get("http://localhost:3001/f1099s");
 }
 
-export function delete1099(id) {
+export async function delete1099(id) {
   return axios.delete(`http://localhost:3001/f1099s/${id}`);
 }
 
-export function save1099(f1099) {
+export async function save1099(f1099) {
   let url = "http://localhost:3001/f1099s/";
   if (f1099.id) url += f1099.id;
   return axios[f1099.id ? "put" : "post"](url, f1099);
